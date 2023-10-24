e03d8c7766f290d5a4bf9f838e6a7462969abda2
Pawan Kumar <Pawankumar@t-a-g.net>
2023-10-24 13:44:39 +0530

    first commit

---
+++ BlazorWebAPIStroedProcedure/Controllers/StudentController.cs
@@ -0,0 +1,118 @@
+﻿using BlazorWebAPIStroedProcedure.DataRepository;
+using BlazorWebAPIStroedProcedure.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BlazorWebAPIStroedProcedure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentRepo _studentRepo;
+        private readonly ILogger _logger;
+
+
+        public StudentController(StudentRepo studentRepo, ILogger<StudentController> logger)
+        {
+            _studentRepo = studentRepo;
+            _logger = logger;
+        }
+
+        [Route("AddStudentData")]
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            if (student != null && ModelState.IsValid)
+            {
+
+                bool success = _studentRepo.InsertStudent(
+                    student.StudentId, student.Gender, student.NationalIty, student.PlaceofBirth,
+                    student.StageId, student.GradeId, student.SectionId, student.Topic,
+                    student.Semester, student.Relation, student.Raisedhands.Value,
+                    student.VisItedResources.Value, student.AnnouncementsView.Value, student.Discussion.Value,
+                    student.ParentAnsweringSurvey, student.ParentschoolSatisfaction, student.StudentAbsenceDays,
+                    student.StudentMarks.Value, student.Class
+                );
+                if (success)
+                {
+                    _logger.LogInformation("Student data is inserted successfully");
+                    return Ok("Student data inserted successfully.");
+                }
+                else
+                {
+                    _logger.LogWarning("Student data not found");
+                    return BadRequest("Failed to insert student.");
+                }
+            }
+            return BadRequest(ModelState);
+
+        }
+
+        [HttpPut("updateStudentData/{id}")]
+        //[HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody] Student student)
+        {
+            bool success = _studentRepo.UpdateStudent(
+                id, student.Gender, student.NationalIty, student.PlaceofBirth,
+                student.StageId, student.GradeId, student.SectionId, student.Topic,
+                student.Semester, student.Relation, student.Raisedhands.Value,
+                student.VisItedResources.Value, student.AnnouncementsView.Value, student.Discussion.Value,
+                student.ParentAnsweringSurvey, student.ParentschoolSatisfaction, student.StudentAbsenceDays,
+                student.StudentMarks.Value, student.Class
+            );
+            if (success)
+            {
+                return Ok("Student updated successfully.");
+            }
+            else
+            {
+                return BadRequest("Failed to update student.");
+            }
+        }
+
+        [Route("getAllStudentData")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Student> students = _studentRepo.GetAllStudents();
+            return Ok(students);
+        }
+
+
+
+        [Route("getStudentDataById")]
+        [HttpGet]
+        //[HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            Student student = _studentRepo.GetStudentByID(id);
+            if (student != null)
+            {
+                return Ok(student);
+            }
+            else
+            {
+                return NotFound("Student not found.");
+            }
+        }
+
+        [Route("deleteStudentData")]
+        [HttpDelete]
+        //[HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            bool success = _studentRepo.DeleteStudentByID(id);
+            if (success)
+            {
+                return Ok("Student deleted successfully.");
+            }
+            else
+            {
+                return BadRequest("Failed to delete student.");
+            }
+        }
+    }
+}
---
+++ BlazorWebAPIStroedProcedure/DataRepository/StudentRepo.cs
@@ -0,0 +1,227 @@
+﻿using BlazorWebAPIStroedProcedure.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BlazorWebAPIStroedProcedure.DataRepository
+{
+    public class StudentRepo
+    {
+        public readonly string _connectionString;
+        public StudentRepo(string connectionString ) { 
+            _connectionString = connectionString;
+        }
+        public bool InsertStudent(
+            string studentId, char gender, string nationality, string placeOfBirth, string stageId,
+            string gradeId, char sectionId, string topic, char semester, string relation,
+            int raisedHands, int visitedResources, int announcementsView, int discussion,
+            string parentAnsweringSurvey, string parentSchoolSatisfaction, string studentAbsenceDays,
+            int studentMarks, char classes)
+            {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("InsertStudent", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Student_ID", studentId);
+                        command.Parameters.AddWithValue("@gender", gender);
+                        command.Parameters.AddWithValue("@NationalITy", nationality);
+                        command.Parameters.AddWithValue("@PlaceOfBirth", placeOfBirth);
+                        command.Parameters.AddWithValue("@StageID", stageId);
+                        command.Parameters.AddWithValue("@GradeID", gradeId);
+                        command.Parameters.AddWithValue("@SectionID", sectionId);
+                        command.Parameters.AddWithValue("@Topic", topic);
+                        command.Parameters.AddWithValue("@Semester", semester);
+                        command.Parameters.AddWithValue("@Relation", relation);
+                        command.Parameters.AddWithValue("@RaisedHands", raisedHands);
+                        command.Parameters.AddWithValue("@VisitedResources", visitedResources);
+                        command.Parameters.AddWithValue("@AnnouncementsView", announcementsView);
+                        command.Parameters.AddWithValue("@Discussion", discussion);
+                        command.Parameters.AddWithValue("@ParentAnsweringSurvey", parentAnsweringSurvey);
+                        command.Parameters.AddWithValue("@ParentschoolSatisfaction", parentSchoolSatisfaction);
+                        command.Parameters.AddWithValue("@StudentAbsenceDays", studentAbsenceDays);
+                        command.Parameters.AddWithValue("@Student_Marks", studentMarks);
+                        command.Parameters.AddWithValue("@Class", classes);
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            }
+
+       public bool UpdateStudent(
+            string studentId, char gender, string nationality, string placeOfBirth, string stageId,
+            string gradeId, char sectionId, string topic, char semester, string relation,
+            int raisedHands, int visitedResources, int announcementsView, int discussion,
+            string parentAnsweringSurvey, string parentSchoolSatisfaction, string studentAbsenceDays,
+            int studentMarks, char classes)
+            {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("UpdateStudent", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Student_ID", studentId);
+                        command.Parameters.AddWithValue("@gender", gender);
+                        command.Parameters.AddWithValue("@NationalITy", nationality);
+                        command.Parameters.AddWithValue("@PlaceOfBirth", placeOfBirth);
+                        command.Parameters.AddWithValue("@StageID", stageId);
+                        command.Parameters.AddWithValue("@GradeID", gradeId);
+                        command.Parameters.AddWithValue("@SectionID", sectionId);
+                        command.Parameters.AddWithValue("@Topic", topic);
+                        command.Parameters.AddWithValue("@Semester", semester);
+                        command.Parameters.AddWithValue("@Relation", relation);
+                        command.Parameters.AddWithValue("@RaisedHands", raisedHands);
+                        command.Parameters.AddWithValue("@VisitedResources", visitedResources);
+                        command.Parameters.AddWithValue("@AnnouncementsView", announcementsView);
+                        command.Parameters.AddWithValue("@Discussion", discussion);
+                        command.Parameters.AddWithValue("@ParentAnsweringSurvey", parentAnsweringSurvey);
+                        command.Parameters.AddWithValue("@ParentschoolSatisfaction", parentSchoolSatisfaction);
+                        command.Parameters.AddWithValue("@StudentAbsenceDays", studentAbsenceDays);
+                        command.Parameters.AddWithValue("@Student_Marks", studentMarks);
+                        command.Parameters.AddWithValue("@Class", classes);
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+               
+            }
+
+        public List<Student> GetAllStudents()
+
+        {
+            try
+            {
+                List<Student> students = new List<Student>();
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("GetAllStudents", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+
+                            {
+                                students.Add(MapStudentFromDataReader(reader));
+                            }
+                        }
+                    }
+                }
+                return students;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public Student GetStudentByID(string studentId)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("GetStudentByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Student_ID", studentId);
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+
+                            {
+                                return MapStudentFromDataReader(reader);
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+        private Student MapStudentFromDataReader(SqlDataReader reader)
+
+        {
+            try
+            {
+                return new Student
+                {
+                    StudentId = reader["Student_ID"].ToString(),
+                    Gender = reader["gender"].ToString()[0],
+                    NationalIty = reader["NationalIty"].ToString(),
+                    PlaceofBirth = reader["PlaceofBirth"].ToString(),
+                    StageId = reader["StageID"].ToString(),
+                    GradeId = reader["GradeID"].ToString(),
+                    SectionId = reader["SectionID"].ToString()[0],
+                    Topic = reader["Topic"].ToString(),
+                    Semester = reader["Semester"].ToString()[0],
+                    Relation = reader["Relation"].ToString(),
+                    Raisedhands = Convert.ToInt32(reader["raisedhands"]),
+                    VisItedResources = Convert.ToInt32(reader["VisITedResources"]),
+                    AnnouncementsView = Convert.ToInt32(reader["AnnouncementsView"]),
+                    Discussion = Convert.ToInt32(reader["Discussion"]),
+                    ParentAnsweringSurvey = reader["ParentAnsweringSurvey"].ToString(),
+                    ParentschoolSatisfaction = reader["ParentschoolSatisfaction"].ToString(),
+                    StudentAbsenceDays = reader["StudentAbsenceDays"].ToString(),
+                    StudentMarks = Convert.ToInt32(reader["Student_Marks"]),
+                    Class = reader["Class"].ToString()[0]
+                };
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public bool DeleteStudentByID(string studentId)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("DeleteStudentByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Student_ID", studentId);
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
